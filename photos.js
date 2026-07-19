a5b8a671a2155eb9a5c6e764dbc39e5d86d48743
Album Updater <album@local>
2026-07-20 00:56:20 +0800

    update: 19 meetings + 39 photos with web thumbnails

---
+++ photos.js
@@ -0,0 +1,162 @@
+window.PHOTO_DATA = {
+  "第十八次": [
+    {
+      "thumb": "web/18/MVIMG_20260420_001749.jpg",
+      "full": "photos/18/MVIMG_20260420_001749.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_001813.jpg",
+      "full": "photos/18/MVIMG_20260420_001813.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_191615.jpg",
+      "full": "photos/18/MVIMG_20260420_191615.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_191617.jpg",
+      "full": "photos/18/MVIMG_20260420_191617.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_191618.jpg",
+      "full": "photos/18/MVIMG_20260420_191618.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_193521.jpg",
+      "full": "photos/18/MVIMG_20260420_193521.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_204906.jpg",
+      "full": "photos/18/MVIMG_20260420_204906.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_204948.jpg",
+      "full": "photos/18/MVIMG_20260420_204948.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_205042.jpg",
+      "full": "photos/18/MVIMG_20260420_205042.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_212146.jpg",
+      "full": "photos/18/MVIMG_20260420_212146.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_212202.jpg",
+      "full": "photos/18/MVIMG_20260420_212202.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_212208.jpg",
+      "full": "photos/18/MVIMG_20260420_212208.jpg"
+    },
+    {
+      "thumb": "web/18/MVIMG_20260420_212229.jpg",
+      "full": "photos/18/MVIMG_20260420_212229.jpg"
+    },
+    {
+      "thumb": "web/18/Screenshot_2026-04-27-00-07-31-052_com.tencent.mm.jpg",
+      "full": "photos/18/Screenshot_2026-04-27-00-07-31-052_com.tencent.mm.jpg"
+    }
+  ],
+  "第十九次": [
+    {
+      "thumb": "web/19/IMG_20260611_160601.jpg",
+      "full": "photos/19/IMG_20260611_160601.jpg"
+    },
+    {
+      "thumb": "web/19/IMG_20260611_181708.jpg",
+      "full": "photos/19/IMG_20260611_181708.jpg"
+    },
+    {
+      "thumb": "web/19/IMG_20260611_181714.jpg",
+      "full": "photos/19/IMG_20260611_181714.jpg"
+    },
+    {
+      "thumb": "web/19/IMG_20260611_181741.jpg",
+      "full": "photos/19/IMG_20260611_181741.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260608_211128.jpg",
+      "full": "photos/19/MVIMG_20260608_211128.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260609_183941.jpg",
+      "full": "photos/19/MVIMG_20260609_183941.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260610_163855.jpg",
+      "full": "photos/19/MVIMG_20260610_163855.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260610_194043.jpg",
+      "full": "photos/19/MVIMG_20260610_194043.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_145902.jpg",
+      "full": "photos/19/MVIMG_20260611_145902.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_151242.jpg",
+      "full": "photos/19/MVIMG_20260611_151242.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_151301.jpg",
+      "full": "photos/19/MVIMG_20260611_151301.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_151749.jpg",
+      "full": "photos/19/MVIMG_20260611_151749.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_151757.jpg",
+      "full": "photos/19/MVIMG_20260611_151757.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_152430.jpg",
+      "full": "photos/19/MVIMG_20260611_152430.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_152456.jpg",
+      "full": "photos/19/MVIMG_20260611_152456.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_152535.jpg",
+      "full": "photos/19/MVIMG_20260611_152535.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_152916.jpg",
+      "full": "photos/19/MVIMG_20260611_152916.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_153004.jpg",
+      "full": "photos/19/MVIMG_20260611_153004.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_154527.jpg",
+      "full": "photos/19/MVIMG_20260611_154527.jpg"
+    },
+    {
+      "thumb": "web/19/MVIMG_20260611_172136_1784277166058edit.jpg",
+      "full": "photos/19/MVIMG_20260611_172136_1784277166058edit.jpg"
+    },
+    {
+      "thumb": "web/19/Screenshot_2026-06-11-18-54-31-339_com.tencent.mm.jpg",
+      "full": "photos/19/Screenshot_2026-06-11-18-54-31-339_com.tencent.mm.jpg"
+    },
+    {
+      "thumb": "web/19/mmexport1784470066884.jpg",
+      "full": "photos/19/mmexport1784470066884.jpg"
+    },
+    {
+      "thumb": "web/19/mmexport1784470077253.jpg",
+      "full": "photos/19/mmexport1784470077253.jpg"
+    },
+    {
+      "thumb": "web/19/mmexport1784470087140.jpg",
+      "full": "photos/19/mmexport1784470087140.jpg"
+    },
+    {
+      "thumb": "web/19/mmexport1784470092975.jpg",
+      "full": "photos/19/mmexport1784470092975.jpg"
+    }
+  ]
+};
