3a91bfcbabd64165620c18db6d64e8bcb9d3e2c6
czf527hst <2845111219@qq.com>
2026-07-23 02:36:50 +0800

    update photos.js

--- photos.js
+++ photos.js
@@ -1,4 +1,498 @@
 window.PHOTO_DATA = {
+  "第十次": [
+    {
+      "thumb": "web/10/IMG_20241015_200112.jpg",
+      "full": "photos/10/IMG_20241015_200112.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241015_215028.jpg",
+      "full": "photos/10/IMG_20241015_215028.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241015_224810.jpg",
+      "full": "photos/10/IMG_20241015_224810.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241015_224827.jpg",
+      "full": "photos/10/IMG_20241015_224827.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_164442.jpg",
+      "full": "photos/10/IMG_20241016_164442.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_191016.jpg",
+      "full": "photos/10/IMG_20241016_191016.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_213349.jpg",
+      "full": "photos/10/IMG_20241016_213349.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_213541.jpg",
+      "full": "photos/10/IMG_20241016_213541.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_213723.jpg",
+      "full": "photos/10/IMG_20241016_213723.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_213919.jpg",
+      "full": "photos/10/IMG_20241016_213919.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_214424.jpg",
+      "full": "photos/10/IMG_20241016_214424.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_214432.jpg",
+      "full": "photos/10/IMG_20241016_214432.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_214914.jpg",
+      "full": "photos/10/IMG_20241016_214914.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_215032.jpg",
+      "full": "photos/10/IMG_20241016_215032.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_215038.jpg",
+      "full": "photos/10/IMG_20241016_215038.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224657.jpg",
+      "full": "photos/10/IMG_20241016_224657.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224736.jpg",
+      "full": "photos/10/IMG_20241016_224736.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224741.jpg",
+      "full": "photos/10/IMG_20241016_224741.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224743.jpg",
+      "full": "photos/10/IMG_20241016_224743.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224750.jpg",
+      "full": "photos/10/IMG_20241016_224750.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224813.jpg",
+      "full": "photos/10/IMG_20241016_224813.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224833.jpg",
+      "full": "photos/10/IMG_20241016_224833.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224844.jpg",
+      "full": "photos/10/IMG_20241016_224844.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_224948.jpg",
+      "full": "photos/10/IMG_20241016_224948.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_225003.jpg",
+      "full": "photos/10/IMG_20241016_225003.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241016_225016.jpg",
+      "full": "photos/10/IMG_20241016_225016.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_143746.jpg",
+      "full": "photos/10/IMG_20241017_143746.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_143753.jpg",
+      "full": "photos/10/IMG_20241017_143753.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_144822.jpg",
+      "full": "photos/10/IMG_20241017_144822.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_145550.jpg",
+      "full": "photos/10/IMG_20241017_145550.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_152037.jpg",
+      "full": "photos/10/IMG_20241017_152037.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_152106.jpg",
+      "full": "photos/10/IMG_20241017_152106.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_153323.jpg",
+      "full": "photos/10/IMG_20241017_153323.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_153338.jpg",
+      "full": "photos/10/IMG_20241017_153338.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_164023.jpg",
+      "full": "photos/10/IMG_20241017_164023.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_164030.jpg",
+      "full": "photos/10/IMG_20241017_164030.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_190101.jpg",
+      "full": "photos/10/IMG_20241017_190101.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_190103.jpg",
+      "full": "photos/10/IMG_20241017_190103.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241017_190323.jpg",
+      "full": "photos/10/IMG_20241017_190323.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241204_225556.jpg",
+      "full": "photos/10/IMG_20241204_225556.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241204_230141.jpg",
+      "full": "photos/10/IMG_20241204_230141.jpg"
+    },
+    {
+      "thumb": "web/10/IMG_20241205_223136.jpg",
+      "full": "photos/10/IMG_20241205_223136.jpg"
+    }
+  ],
+  "第十一次": [
+    {
+      "thumb": "web/11/IMG_20250117_081029.jpg",
+      "full": "photos/11/IMG_20250117_081029.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_155409.jpg",
+      "full": "photos/11/IMG_20250119_155409.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_155420.jpg",
+      "full": "photos/11/IMG_20250119_155420.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_155946.jpg",
+      "full": "photos/11/IMG_20250119_155946.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_155953.jpg",
+      "full": "photos/11/IMG_20250119_155953.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160033.jpg",
+      "full": "photos/11/IMG_20250119_160033.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160327.jpg",
+      "full": "photos/11/IMG_20250119_160327.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160333.jpg",
+      "full": "photos/11/IMG_20250119_160333.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160337.jpg",
+      "full": "photos/11/IMG_20250119_160337.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160343.jpg",
+      "full": "photos/11/IMG_20250119_160343.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160502.jpg",
+      "full": "photos/11/IMG_20250119_160502.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160519.jpg",
+      "full": "photos/11/IMG_20250119_160519.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160826.jpg",
+      "full": "photos/11/IMG_20250119_160826.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160919.jpg",
+      "full": "photos/11/IMG_20250119_160919.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160923.jpg",
+      "full": "photos/11/IMG_20250119_160923.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160926.jpg",
+      "full": "photos/11/IMG_20250119_160926.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_160926_1.jpg",
+      "full": "photos/11/IMG_20250119_160926_1.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_161544.jpg",
+      "full": "photos/11/IMG_20250119_161544.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_161628.jpg",
+      "full": "photos/11/IMG_20250119_161628.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_161638.jpg",
+      "full": "photos/11/IMG_20250119_161638.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_170959.jpg",
+      "full": "photos/11/IMG_20250119_170959.jpg"
+    },
+    {
+      "thumb": "web/11/IMG_20250119_195901.jpg",
+      "full": "photos/11/IMG_20250119_195901.jpg"
+    }
+  ],
+  "第十二次": [
+    {
+      "thumb": "web/12/IMG_20250214_210508.jpg",
+      "full": "photos/12/IMG_20250214_210508.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250214_210643.jpg",
+      "full": "photos/12/IMG_20250214_210643.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_145900.jpg",
+      "full": "photos/12/IMG_20250215_145900.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_145914.jpg",
+      "full": "photos/12/IMG_20250215_145914.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_145929.jpg",
+      "full": "photos/12/IMG_20250215_145929.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_145937.jpg",
+      "full": "photos/12/IMG_20250215_145937.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_145940.jpg",
+      "full": "photos/12/IMG_20250215_145940.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_145948.jpg",
+      "full": "photos/12/IMG_20250215_145948.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_152733.jpg",
+      "full": "photos/12/IMG_20250215_152733.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_181014.jpg",
+      "full": "photos/12/IMG_20250215_181014.jpg"
+    },
+    {
+      "thumb": "web/12/IMG_20250215_193238.jpg",
+      "full": "photos/12/IMG_20250215_193238.jpg"
+    }
+  ],
+  "第十四次": [
+    {
+      "thumb": "web/14/IMG_20250706_190057.jpg",
+      "full": "photos/14/IMG_20250706_190057.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_210001.jpg",
+      "full": "photos/14/IMG_20250706_210001.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_210314.jpg",
+      "full": "photos/14/IMG_20250706_210314.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_211442.jpg",
+      "full": "photos/14/IMG_20250706_211442.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_211452.jpg",
+      "full": "photos/14/IMG_20250706_211452.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_214630.jpg",
+      "full": "photos/14/IMG_20250706_214630.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_214712.jpg",
+      "full": "photos/14/IMG_20250706_214712.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_214716.jpg",
+      "full": "photos/14/IMG_20250706_214716.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_214718.jpg",
+      "full": "photos/14/IMG_20250706_214718.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250706_215808.jpg",
+      "full": "photos/14/IMG_20250706_215808.jpg"
+    },
+    {
+      "thumb": "web/14/IMG_20250707_121037.jpg",
+      "full": "photos/14/IMG_20250707_121037.jpg"
+    }
+  ],
+  "第十五次": [
+    {
+      "thumb": "web/15/IMG_20251021_163612.jpg",
+      "full": "photos/15/IMG_20251021_163612.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251021_163616.jpg",
+      "full": "photos/15/IMG_20251021_163616.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251021_163624.jpg",
+      "full": "photos/15/IMG_20251021_163624.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251022_090840.jpg",
+      "full": "photos/15/IMG_20251022_090840.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251022_143057.jpg",
+      "full": "photos/15/IMG_20251022_143057.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251022_152029.jpg",
+      "full": "photos/15/IMG_20251022_152029.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251022_162123.jpg",
+      "full": "photos/15/IMG_20251022_162123.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251022_162124.jpg",
+      "full": "photos/15/IMG_20251022_162124.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251022_174851.jpg",
+      "full": "photos/15/IMG_20251022_174851.jpg"
+    },
+    {
+      "thumb": "web/15/IMG_20251022_174903.jpg",
+      "full": "photos/15/IMG_20251022_174903.jpg"
+    }
+  ],
+  "第十六次": [
+    {
+      "thumb": "web/16/IMG_20260127_151229.jpg",
+      "full": "photos/16/IMG_20260127_151229.jpg"
+    },
+    {
+      "thumb": "web/16/IMG_20260127_215639.jpg",
+      "full": "photos/16/IMG_20260127_215639.jpg"
+    },
+    {
+      "thumb": "web/16/IMG_20260128_142048.jpg",
+      "full": "photos/16/IMG_20260128_142048.jpg"
+    },
+    {
+      "thumb": "web/16/IMG_20260128_164842.jpg",
+      "full": "photos/16/IMG_20260128_164842.jpg"
+    },
+    {
+      "thumb": "web/16/IMG_20260128_175258.jpg",
+      "full": "photos/16/IMG_20260128_175258.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260126_205409.jpg",
+      "full": "photos/16/MVIMG_20260126_205409.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260126_212355.jpg",
+      "full": "photos/16/MVIMG_20260126_212355.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260127_152152.jpg",
+      "full": "photos/16/MVIMG_20260127_152152.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260127_154322.jpg",
+      "full": "photos/16/MVIMG_20260127_154322.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260127_215322.jpg",
+      "full": "photos/16/MVIMG_20260127_215322.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260127_215331.jpg",
+      "full": "photos/16/MVIMG_20260127_215331.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260128_164733.jpg",
+      "full": "photos/16/MVIMG_20260128_164733.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260128_165150.jpg",
+      "full": "photos/16/MVIMG_20260128_165150.jpg"
+    },
+    {
+      "thumb": "web/16/MVIMG_20260128_170402.jpg",
+      "full": "photos/16/MVIMG_20260128_170402.jpg"
+    },
+    {
+      "thumb": "web/16/Screenshot_2026-01-28-14-22-11-154_com.miui.gallery.jpg",
+      "full": "photos/16/Screenshot_2026-01-28-14-22-11-154_com.miui.gallery.jpg"
+    }
+  ],
+  "第十七次": [
+    {
+      "thumb": "web/17/MVIMG_20260325_201722.jpg",
+      "full": "photos/17/MVIMG_20260325_201722.jpg"
+    },
+    {
+      "thumb": "web/17/MVIMG_20260325_222950.jpg",
+      "full": "photos/17/MVIMG_20260325_222950.jpg"
+    },
+    {
+      "thumb": "web/17/MVIMG_20260326_172243.jpg",
+      "full": "photos/17/MVIMG_20260326_172243.jpg"
+    },
+    {
+      "thumb": "web/17/MVIMG_20260326_172253.jpg",
+      "full": "photos/17/MVIMG_20260326_172253.jpg"
+    },
+    {
+      "thumb": "web/17/MVIMG_20260326_205641.jpg",
+      "full": "photos/17/MVIMG_20260326_205641.jpg"
+    },
+    {
+      "thumb": "web/17/MVIMG_20260326_205706_1775799368877edit.jpg",
+      "full": "photos/17/MVIMG_20260326_205706_1775799368877edit.jpg"
+    },
+    {
+      "thumb": "web/17/MVIMG_20260326_205708.jpg",
+      "full": "photos/17/MVIMG_20260326_205708.jpg"
+    },
+    {
+      "thumb": "web/17/MVIMG_20260326_205710.jpg",
+      "full": "photos/17/MVIMG_20260326_205710.jpg"
+    },
+    {
+      "thumb": "web/17/mmexport1784741636337.jpg",
+      "full": "photos/17/mmexport1784741636337.jpg"
+    }
+  ],
   "第十八次": [
     {
       "thumb": "web/18/MVIMG_20260420_001749.jpg",
@@ -37,14 +531,6 @@ window.PHOTO_DATA = {
       "full": "photos/18/MVIMG_20260420_205042.jpg"
     },
     {
-      "thumb": "web/18/MVIMG_20260420_212146.jpg",
-      "full": "photos/18/MVIMG_20260420_212146.jpg"
-    },
-    {
-      "thumb": "web/18/MVIMG_20260420_212202.jpg",
-      "full": "photos/18/MVIMG_20260420_212202.jpg"
-    },
-    {
       "thumb": "web/18/MVIMG_20260420_212208.jpg",
       "full": "photos/18/MVIMG_20260420_212208.jpg"
     },
@@ -79,16 +565,8 @@ window.PHOTO_DATA = {
       "full": "photos/19/MVIMG_20260608_211128.jpg"
     },
     {
-      "thumb": "web/19/MVIMG_20260609_183941.jpg",
-      "full": "photos/19/MVIMG_20260609_183941.jpg"
-    },
-    {
       "thumb": "web/19/MVIMG_20260610_163855.jpg",
       "full": "photos/19/MVIMG_20260610_163855.jpg"
-    },
-    {
-      "thumb": "web/19/MVIMG_20260610_194043.jpg",
-      "full": "photos/19/MVIMG_20260610_194043.jpg"
     },
     {
       "thumb": "web/19/MVIMG_20260611_145902.jpg",
